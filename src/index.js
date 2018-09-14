0455dccdf06980b8c237870ef464b6ccd1041dc7
mobidiic <sigus128@gmail.com>
2018-09-14 11:59:28 +0900

    initiating the project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+
+window.React = React
+
+render(
+  <App />,
+  document.getElementById('react-container')
+)
---
+++ webpack.prod.config.js
@@ -0,0 +1,78 @@
+var webpack = require("webpack")
+var path = require("path")
+var CleanWebpackPlugin = require('clean-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+process.noDeprecation = true
+
+module.exports = {
+    entry: ['babel-polyfill', "./src/index.js"],
+    output: {
+        path:path.join(__dirname, 'dist'),
+        filename: "bundle.js"
+    },
+    optimization:{
+      minimizer: [
+        new UglifyJsPlugin({
+          sourceMap: false
+        })
+      ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/env', '@babel/react']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader',
+                  { loader: 'css-loader',
+                    options: {
+                      importLoaders: 1,
+                      module: true,
+                      localIdentName: '[name]__[local]__[hash: base64:5]'
+                    }},
+                  { loader: 'postcss-loader',
+                    options: {
+                      plugins: () => [require('autoprefixer')]
+                    }}]
+            },
+            {
+                test: /\.scss/,
+                use: ['style-loader',
+                  { loader: 'css-loader',
+                    options: {
+                      importLoaders: 1,
+                      module: true,
+                      localIdentName: '[name]__[local]__[hash: base64:5]'
+                    }},
+                  { loader: 'postcss-loader',
+                    options: {
+                      plugins: () => [require('autoprefixer')]
+                    }}, 'sass-loader']
+            },
+            { test: /\.(png|woff|woff2|eot|ttf|svg|jpg|jpeg)$/, loader: 'url-loader?limit=100000' }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Cean Park',
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
