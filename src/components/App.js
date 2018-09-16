3164a37841b0aa369e181a6d0f91999aeca0f0dd
mobidiic <sigus128@gmail.com>
2018-09-16 17:45:31 +0900

    feat: fade-in 애니메이션 추가 중간저장

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,29 @@ import profileCharacter from '../../imgs/myCharacter.png'
 import '../../stylesheets/App.scss'
 
 class App extends Component {
+
+  componentDidMount(){
+    // let hiMessage = document.getElementById('hiMessage')
+    // let contact = document.getElementById('contact-wrapper')
+    const handleFirstReveal = () => {
+      let upside = document.getElementById('upside')
+      let opacity = upside.style.opacity
+      opacity = '0'
+      let animate = setInterval(()=>{
+          opacity = parseFloat(opacity) + 0.05 + ''
+      }, 1000)
+      if (parseFloat(opacity) >= 1){
+        clearInterval(animate);
+      }
+    }
+      handleFirstReveal()
+  }
+
   render(){
     return(
       <div className="profile-wrapper">
           <div className="profile">
-          <div className="upside">
+          <div id="upside">
             <div className="img-wrapper">
               <img src={profileCharacter} />
             </div>
@@ -17,12 +35,12 @@ class App extends Component {
               <Personal />
             </div>
           </div>
-          <div className="hi-wrapper">
-            <div>
+          <div id="hi-wrapper">
+            <div id="hiMessage">
               I want to live the world <span>more easy</span>, and I believe that we can have <span>better experience</span> in our life through the web and the app.
             </div>
           </div>
-          <div className="contact-wrapper">
+          <div id="contact-wrapper">
             <Contact />
           </div>
         </div>
